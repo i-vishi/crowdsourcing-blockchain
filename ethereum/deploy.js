2a829bbcb352840ec113c460a005034094f78268
i-vishi <rr.vishalgaur@gmail.com>
2020-12-13 23:22:59 +0530

    added deployment script

---
+++ ethereum/deploy.js
@@ -0,0 +1,19 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+const config = require("../config.json");
+
+const provider = new HDWalletProvider(config.mnemonic, config.infuraAPI);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("attempting to connect to ", accounts[0]);
+
+  const result = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({ data: `0x${compiledFactory.evm.bytecode.object}` }) // add bytecode
+    .send({ from: accounts[0] }); // remove gas
+
+  console.log("contract deployed to", result.options.address);
+};
+deploy();
